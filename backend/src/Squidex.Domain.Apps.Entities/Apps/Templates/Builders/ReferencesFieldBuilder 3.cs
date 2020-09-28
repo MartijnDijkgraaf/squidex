50e265c91e7c75bd46952e95b1c3f73b508b2af1
MartijnDijkgraaf <martijndijkgraaf96@gmail.com>
2020-09-28 09:55:43 +0200

    git merge master upstream into local master

---
+++ backend/src/Squidex.Domain.Apps.Entities/Apps/Templates/Builders/ReferencesFieldBuilder 3.cs
@@ -0,0 +1,28 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+using Squidex.Domain.Apps.Core.Schemas;
+using Squidex.Domain.Apps.Entities.Schemas.Commands;
+
+namespace Squidex.Domain.Apps.Entities.Apps.Templates.Builders
+{
+    public class ReferencesFieldBuilder : FieldBuilder
+    {
+        public ReferencesFieldBuilder(UpsertSchemaField field, UpsertCommand schema)
+            : base(field, schema)
+        {
+        }
+
+        public ReferencesFieldBuilder WithSchemaId(Guid id)
+        {
+            Properties<ReferencesFieldProperties>().SchemaId = id;
+
+            return this;
+        }
+    }
+}
